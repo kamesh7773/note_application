562e1ab1dee1566e660b85aa169bdac4af842fc9
kamesh7773 <kameshsinghaaa64@gmail.com>
2024-07-10 23:00:03 +0530

    COMMIT FROM LIBARY

--- lib/providers/layout_change_provider.dart
+++ lib/providers/layout_change_provider.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LayoutChangeProvider extends ChangeNotifier {
   bool _isGridView = true;
 
   bool get isGridView => _isGridView;
 
-  void changeLayout() {
+  void changeLayout() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
     _isGridView = !_isGridView;
+
+    await prefs.setBool('isGridView', _isGridView);
+
     notifyListeners();
   }
 }
